4a0917d2218b27672061a6bfc19e762be697e7ae
krystian <nlins8224@gmail.com>
2021-12-22 21:11:46 +0100

    Add prettier, run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.less';
 
-import "./database/firebase"
-import { LiveCoinsReceiver } from "./components/LiveCoins/LiveCoinsReceiver";
+import './database/firebase';
+import { LiveCoinsReceiver } from './components/LiveCoins/LiveCoinsReceiver';
 
 function App() {
-    return (
-        <div className="root">
-            <LiveCoinsReceiver />
-        </div>
-    );
+  return (
+    <div className="root">
+      <LiveCoinsReceiver />
+    </div>
+  );
 }
 
 export default App;
--- src/components/LiveCoins/LiveCoinsTable.tsx
+++ src/components/LiveCoins/LiveCoinsTable.tsx
@@ -1,54 +1,52 @@
-import React from "react";
-import {Table} from "antd";
-import {Asset} from "../../Asset";
-import "antd/dist/antd.css"
+import React from 'react';
+import { Table } from 'antd';
+import { Asset } from '../../Asset';
+import 'antd/dist/antd.css';
 
 const columns: any = [
-    {
-        title: 'Symbol',
-        dataIndex: 'symbol',
-        key: 'symbol',
-    },
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Price',
-        dataIndex: 'price',
-        key: 'price',
-    },
-    {
-        title: 'Price Change',
-        dataIndex: 'price_change',
-        key: 'price_change',
-    },
-    {
-        title: '% Price Change',
-        dataIndex: 'price_change_percentage',
-        key: 'price_change_percentage',
-    },
-    {
-        title: 'High',
-        dataIndex: 'high',
-        key: 'high',
-    },
-    {
-        title: 'Low',
-        dataIndex: 'low',
-        key: 'low',
-    },
+  {
+    title: 'Symbol',
+    dataIndex: 'symbol',
+    key: 'symbol',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+  },
+  {
+    title: 'Price Change',
+    dataIndex: 'price_change',
+    key: 'price_change',
+  },
+  {
+    title: '% Price Change',
+    dataIndex: 'price_change_percentage',
+    key: 'price_change_percentage',
+  },
+  {
+    title: 'High',
+    dataIndex: 'high',
+    key: 'high',
+  },
+  {
+    title: 'Low',
+    dataIndex: 'low',
+    key: 'low',
+  },
 ];
 
-export function LiveCoinsTable(props: { assets : Array<Asset> }): JSX.Element {
-    return <>
-        <div>
-            <Table
-                dataSource={props.assets}
-                columns={columns}
-                pagination={false}
-            />
-        </div>
+export function LiveCoinsTable(props: { assets: Array<Asset> }): JSX.Element {
+  return (
+    <>
+      <div>
+        <Table dataSource={props.assets} columns={columns} pagination={false} />
+      </div>
     </>
+  );
 }
