9dff7f41883a746b91dd6851257f82947601d135
krystian <nlins8224@gmail.com>
2022-01-10 19:49:00 +0100

    Add sorter to watchlist table

--- src/components/Watchlist/config/columns.tsx
+++ src/components/Watchlist/config/columns.tsx
@@ -2,92 +2,55 @@ import IRenderText from "../../../interfaces/RenderText";
 import {Link} from "react-router-dom";
 import React from "react";
 
-export const columnsVolume: any = [
-    {
-        title: 'Symbol',
-        dataIndex: 'symbol',
-        key: 'symbol',
-        render: (text: IRenderText, record: { symbol: string; }) => <Link to={record.symbol}>{text}</Link>
-    },
-    {
-        title: 'Volume',
-        dataIndex: 'total_volume',
-        key: 'volume',
-    },
-    {
-        title: 'Change',
-        dataIndex: 'price_change',
-        key: 'change',
-    },
-    {
-        title: '% Change',
-        dataIndex: 'price_change_percentage',
-        key: '% change',
-    },
-];
-
-export const columnsPrice: any = [
-    {
-        title: 'Symbol',
-        dataIndex: 'symbol',
-        key: 'symbol',
-        render: (text: IRenderText, record: { symbol: string; }) => <Link to={record.symbol}>{text}</Link>
-    },
-    {
-        title: 'Price',
-        dataIndex: 'price',
-        key: 'price',
-    },
-    {
-        title: 'Change',
-        dataIndex: 'price_change',
-        key: 'change',
-    },
-    {
-        title: '% Change',
-        dataIndex: 'price_change_percentage',
-        key: '% change',
-    },
-];
-
 export const columns: any = [
     {
         title: 'Symbol',
         dataIndex: 'symbol',
         key: 'symbol',
-        render: (text: IRenderText, record: { symbol: string; }) => <Link to={record.symbol}>{text}</Link>
+        render: (text: IRenderText, record: { symbol: string; }) => <Link to={record.symbol}>{text}</Link>,
+        sorter: (a: { symbol: string; }, b: { symbol: any; }) => a.symbol.localeCompare(b.symbol)
     },
     {
         title: 'Name',
         dataIndex: 'name',
         key: 'name',
+        sorter: (a: { name: string; }, b: { name: any; }) => a.name.localeCompare(b.name)
     },
     {
         title: 'Price',
         dataIndex: 'price',
         key: 'price',
+        sorter: (a: { price: number; }, b: { price: number; }) => a.price - b.price
+
     },
     {
         title: 'Price Change',
         dataIndex: 'price_change',
         key: 'price_change',
+        sorter: (a: { price_change: number; }, b: { price_change: number; }) => a.price_change - b.price_change
     },
     {
         title: '% Price Change',
         dataIndex: 'price_change_percentage',
         key: 'price_change_percentage',
+        sorter: (a: { price_change_percentage: number; }, b: { price_change_percentage: number; }) => a.price_change_percentage - b.price_change_percentage
+
     },
     {
         title: 'High',
         dataIndex: 'high',
         key: 'high',
+        sorter: (a: { high: number; }, b: { high: number; }) => a.high - b.high
+
     },
     {
         title: 'Low',
         dataIndex: 'low',
         key: 'low',
+        sorter: (a: { low: number; }, b: { low: number; }) => a.low - b.low
+
     },
 ];
 
-export default {columns, columnsPrice, columnsVolume}
+export default {columns}
 
