fbc3f6dae790ae4bdd4d632f1f50dca128e19f8c
krystian <nlins8224@gmail.com>
2022-01-12 02:43:35 +0100

    Make watchlist responsive

--- src/components/Watchlist/WatchlistTableView.tsx
+++ src/components/Watchlist/WatchlistTableView.tsx
@@ -29,7 +29,7 @@ const WatchlistTableView: () => JSX.Element = () => {
     return (
         <div>
             <Row align="middle">
-                <Col xs={{ span: 12, offset: 3 }} lg={{ span: 15, offset: 4 }}>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={{ span: 15, offset: 4}}>
                     <LiveAssetsTable
                         assets={filteredAssets}
                         title={'Watchlist'}
