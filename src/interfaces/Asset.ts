6503a772a0852e5c2929fa360fb7abafe5589f16
krystian <nlins8224@gmail.com>
2022-01-21 20:04:35 +0100

    format AssetInfoPanel

--- src/interfaces/Asset.ts
+++ src/interfaces/Asset.ts
@@ -1,12 +1,12 @@
 export default interface IAsset {
-  high?: number;
-  low?: number;
+  high: number;
+  low: number;
   id?: string;
   last_updated: string;
   market_cap: number;
-  market_cap_change?: number;
-  market_cap_rank?: number;
-  name?: string;
+  market_cap_change: number;
+  market_cap_rank: number;
+  name: string;
   price: number;
   price_change: number;
   price_change_percentage: number;
---
+++ src/priceFormatter.ts
@@ -0,0 +1,7 @@
+export const priceFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 4,
+});
