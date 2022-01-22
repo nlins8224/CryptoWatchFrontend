68b7d35a5efbe9155442d587425225b1f0d0dffc
krystian <nlins8224@gmail.com>
2022-01-22 15:20:05 +0100

    Add spinner, disable hover

--- src/components/LiveAssets/children/LiveChartsTable.tsx
+++ src/components/LiveAssets/children/LiveChartsTable.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Table } from 'antd';
+import {ConfigProvider, Empty, Spin, Table} from 'antd';
 import 'antd/dist/antd.css';
 import Text from 'antd/es/typography/Text';
 import { v4 as uuidv4 } from 'uuid';
@@ -9,6 +9,10 @@ interface ChartData {
     chart_data: number[][];
     symbol: string;
 }
+
+const locale = {
+    emptyText: 'Data is loading. Please wait.',
+};
 
 const LiveChartsTable = (props: {
     title: string;
@@ -37,12 +41,15 @@ const LiveChartsTable = (props: {
     return (
         <>
             <div>
-                <Text>{props.title}</Text>
-                <Table
-                    dataSource={chartsData}
-                    columns={props.columns}
-                    pagination={false}
-                />
+                <ConfigProvider renderEmpty={() => <Spin/>}>
+                    <Text>{props.title}</Text>
+                    <Table
+                        dataSource={chartsData}
+                        columns={props.columns}
+                        pagination={false}
+                        className="live-charts-table"
+                    />
+                </ConfigProvider>
             </div>
         </>
     );
